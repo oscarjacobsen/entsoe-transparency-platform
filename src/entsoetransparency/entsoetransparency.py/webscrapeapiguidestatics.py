9d7c46c522136821d41f8702f0a8418890b1fd70
Oscar Jacobsen <67653883+ocrj@users.noreply.github.com>
2021-08-14 11:27:28 +0200

    added files

---
+++ src/entsoetransparency/entsoetransparency.py/webscrapeapiguidestatics.py
@@ -0,0 +1,6 @@
+# Webscrapter for getting updated data for use from api guide url.
+
+def webscrape_url_apiguide(savepath=''):
+    '''Webscraper function for getting updated api statics from guide url and save to file.'''
+
+    
